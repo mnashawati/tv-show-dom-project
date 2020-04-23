cc3f613dca0f04a82cf5f7b06cb75885b6a97863
Mo Nashawati <mo.nashawati@gmail.com>
2020-04-23 19:45:17 +0100

    Level 200 the live search input + refactoring code

--- script.js
+++ script.js
@@ -2,42 +2,73 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  displayingNumOfEpisodes(allEpisodes, allEpisodes);
 }
 
-function makePageForEpisodes(episode) {
+// rootElem.textContent = `Got ${episode.length} episode(s)`;
+
+function createEpisodeCard(episode) {
   const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episode.length} episode(s)`;
 
-  episode.forEach((episode) => {
-    let episodeContainer = document.createElement("div");
-    episodeContainer.className = "episode-container";
-    rootElem.appendChild(episodeContainer);
+  const episodeContainer = document.createElement("div");
+  episodeContainer.className = "episode-container";
+  rootElem.appendChild(episodeContainer);
 
-    let episodeName = episode.name;
-    let episodeNumber = `S${String(episode.season).padStart(2, 0)}E${String(
-      episode.number
-    ).padStart(2, 0)}`;
-    let episodeImage = episode.image.medium.replace("http", "https");
-    let episodeSummary = episode.summary.replace("<p>", "").replace("</p>", "");
+  const titleContainer = document.createElement("div");
+  titleContainer.className = "title-container";
+  episodeContainer.appendChild(titleContainer);
 
-    let titleContainer = document.createElement("div");
-    episodeContainer.appendChild(titleContainer);
-    titleContainer.className = "title-container";
+  const titleElm = document.createElement("h3");
+  titleElm.className = "title";
+  titleContainer.appendChild(titleElm);
 
-    let titleElm = document.createElement("h3");
-    titleContainer.appendChild(titleElm);
-    titleElm.textContent = `${episodeName} - ${episodeNumber}`;
-    titleElm.className = "title";
+  const episodeName = episode.name;
+  const episodeNumber = `S${String(episode.season).padStart(2, 0)}E${String(
+    episode.number
+  ).padStart(2, 0)}`;
+  titleElm.textContent = `${episodeName} - ${episodeNumber}`;
 
-    let img = document.createElement("img");
-    episodeContainer.appendChild(img);
-    img.src = episodeImage;
+  const imgElm = document.createElement("img");
+  episodeContainer.appendChild(imgElm);
+  imgElm.src = episode.image.medium.replace("http", "https");
 
-    let summary = document.createElement("p");
-    episodeContainer.appendChild(summary);
-    summary.innerHTML = episodeSummary;
-    summary.className = "summary";
+  const summaryElm = document.createElement("p");
+  episodeContainer.appendChild(summaryElm);
+  summaryElm.innerHTML = episode.summary.replace("<p>", "").replace("</p>", "");
+  summaryElm.className = "summary";
+}
+
+function makePageForEpisodes(episodes) {
+  episodes.forEach((episode) => {
+    createEpisodeCard(episode);
   });
+}
+
+// Level 200
+const searchElm = document.getElementById("search");
+searchElm.addEventListener("input", () => {
+  document.getElementById("root").innerHTML = "";
+
+  const allEpisodes = getAllEpisodes();
+  const filteredEpisodes = filteredSearch(allEpisodes, searchElm.value);
+
+  makePageForEpisodes(filteredEpisodes);
+  displayingNumOfEpisodes(filteredEpisodes, allEpisodes);
+});
+
+function filteredSearch(episodes, searchInput) {
+  let searchResults = episodes.filter(
+    (episode) =>
+      episode.summary.toUpperCase().includes(searchInput.toUpperCase()) ||
+      episode.name.toUpperCase().includes(searchInput.toUpperCase())
+  );
+  return searchResults;
+}
+
+function displayingNumOfEpisodes(array1, array2) {
+  document.querySelector(
+    ".display"
+  ).textContent = `Displaying: ${array1.length}/${array2.length}`;
 }
 
 window.onload = setup;
